c9d5701956810674cf0309618039893625d3422e
Abhi7230 <105153898+Abhi7230@users.noreply.github.com>
2024-07-27 02:04:06 +0530

    Update AddProblem.jsx

--- src/components/admin/AddProblem.jsx
+++ src/components/admin/AddProblem.jsx
@@ -123,7 +123,7 @@ const AddProblem = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:2999/addproblem", {
+      const response = await fetch("https://backend-last-v2sp.vercel.app/addproblem", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
