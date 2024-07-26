4b3b4234b9d777605a5af2c75608254fff21497c
Abhi7230 <105153898+Abhi7230@users.noreply.github.com>
2024-07-27 01:47:30 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
 
   const loginUser = async () => {
     try {
-      const response = await fetch(`http://localhost:2999/login`, {
+      const response = await fetch(`https://backend-last-v2sp.vercel.app/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
