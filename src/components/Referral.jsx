f8aeee628fe42c298987bee679271e870fac243e
Abhi7230 <105153898+Abhi7230@users.noreply.github.com>
2024-08-01 00:04:41 +0530

    Update Referral.jsx

--- src/components/Referral.jsx
+++ src/components/Referral.jsx
@@ -25,7 +25,7 @@ const Referral = () => {
     try {
       setIsLoading(true);
       const token = Cookies.get('authToken');
-      const response = await fetch('https://backend-last-v2sp.vercel.app/user/referral', {
+      const response = await fetch('https://backend-render-3iug.onrender.com/user/referral', {
         headers: {
           'Authorization': `Bearer ${token}`
         }
