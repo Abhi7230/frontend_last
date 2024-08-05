1c2cfa3a3eddead2659c5db17a722f1a1c0413c8
Abhi7230 <105153898+Abhi7230@users.noreply.github.com>
2024-08-06 00:49:47 +0530

    Update AddProblem.jsx

--- src/components/admin/AddProblem.jsx
+++ src/components/admin/AddProblem.jsx
@@ -123,7 +123,7 @@ const AddProblem = () => {
     };
 
     try {
-      const response = await fetch("https://backend-render-3iug.onrender.com/addproblem", {
+      const response = await fetch("https://backend-last.vercel.app/addproblem", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
