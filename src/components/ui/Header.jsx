e0cb5ced7743bfc5d02f5569886be7e5c9c0582e
Abhi7230 <105153898+Abhi7230@users.noreply.github.com>
2024-08-06 00:51:36 +0530

    Update Header.jsx

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -77,7 +77,7 @@ const Header = () => {
 
   async function getUserDetails() {
     try {
-      const response = await fetch('https://backend-last-v2sp.vercel.app/userdetails', {
+      const response = await fetch('https://backend-last.vercel.app/userdetails', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
